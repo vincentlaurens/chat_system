3e29ad3d071d222fe5b0c6a39862581943a17f01
Vincent.Laurens <Laurensvincent31@gmail.com>
2017-12-01 19:07:18 +0100

    interfaces ok

--- src/main/java/com/vincentlaurens/insa/ui/view/DialoguePageViewer.java
+++ src/main/java/com/vincentlaurens/insa/ui/view/DialoguePageViewer.java
@@ -2,18 +2,17 @@ package com.vincentlaurens.insa.ui.view;
 
 
 import com.vincentlaurens.insa.ui.controller.DialoguePageController;
-import javafx.scene.layout.GridPane;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 
 public class DialoguePageViewer extends JFrame{
     private  DialoguePageController presenter = new DialoguePageController();
-    private  JPanel chat = new JPanel();
-    private JPanel contentPane = new JPanel();
-    private  JTree usersConnectees = new JTree();;
-    private  JTextField affichageMessage = new JTextField();
-    private  JTextField ecrireMessage = new JTextField();
+    private JTextArea textArea;
+    private JTextField inputTextField;
+    private JButton sendButton;
+    private JTree listeUsersConnectes;
 
     public DialoguePageViewer(String pseudo)throws HeadlessException {
         super();
@@ -21,35 +20,31 @@ public class DialoguePageViewer extends JFrame{
 
     }
     private void build(String pseudo){
-        setTitle("Chat System : Bonjour " + pseudo); //On donne un titre à l'application
-        setSize(320,240); //On donne une taille à notre fenêtre
-        setLocationRelativeTo(null); //On centre la fenêtre sur l'écran
-        setResizable(false); //On interdit la redimensionnement de la fenêtre
+        setTitle("Chat System : Bonjour " + pseudo);
+        setSize(600,300);
+        setLocationRelativeTo(null);
+        setResizable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
-        this.usersConnectees.setRootVisible(true);
-        this.usersConnectees.setSize(100,100);
+        textArea = new JTextArea(20, 50);
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        add(new JScrollPane(textArea), BorderLayout.CENTER);
 
-        this.affichageMessage.setEditable(false);
-        this.affichageMessage.setSize(300,70);
-        this.ecrireMessage.setEditable(true);
-        this.ecrireMessage.setSize(300, 30);
+        Box box = Box.createHorizontalBox();
+        add(box, BorderLayout.SOUTH);
+        inputTextField = new JTextField();
+        sendButton = new JButton("Envoyer");
+        box.add(inputTextField);
+        box.add(sendButton);
 
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Utilisateurs Connectés");
 
-        this.chat.add(this.affichageMessage, JFrame.TOP_ALIGNMENT);
-        this.chat.add(this.ecrireMessage, JFrame.BOTTOM_ALIGNMENT);
-        this.chat.setVisible(true);
+        listeUsersConnectes = new JTree(root);
+        add(new JScrollPane(listeUsersConnectes), BorderLayout.WEST);
 
-
-        this.contentPane.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-        this.contentPane.add(this.usersConnectees, JFrame.LEFT_ALIGNMENT);
-        this.contentPane.add(this.chat,JFrame.RIGHT_ALIGNMENT);
-
-
-
-        this.setContentPane(this.contentPane);
 
 }
 
